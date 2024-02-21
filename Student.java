58378de14db45d836b2da5e880d2b0fe2ee7b1d1
126CC <113187762+126CC@users.noreply.github.com>
2024-02-21 12:39:28 -0500

    adding methods

--- Student.java
+++ Student.java
@@ -1,3 +1,29 @@
+import java.util.ArrayList;
 public class Student {
+    private String name;
+    public Student(String name) {
+        this.name = "null";
+    }
 
+    public void addAssignment(String assignName) {
+        ArrayList<String> unGraded = new ArrayList<>();
+        unGraded.add(assignName);
+
+    }
+
+    public void gradeAssignment(String assignment, double grade) {
+
+    }
+
+    public double getAssignmentGrade(String assignment) {
+
+    }
+
+    public double getOverallGrade() {
+
+    }
+
+    public String toString() {
+        
+    }
 }
