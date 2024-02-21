5d3df7b0dec2f70ad5d4708c2ac55f9539581ea8
126CC <113187762+126CC@users.noreply.github.com>
2024-02-21 12:54:22 -0500

    xx

--- Student.java
+++ Student.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 public class Student {
     private String name;
+    private String assignName;
     ArrayList<String> unGraded = new ArrayList<>();
+    ArrayList<String> graded = new ArrayList<>();
     public Student(String name) {
         this.name = "null";
     }
@@ -12,7 +14,9 @@ public class Student {
     }
 
     public void gradeAssignment(String assignment, double grade) {
-        
+        unGraded.remove(assignment);
+        graded.add(assignment);
+
     }
 
     public double getAssignmentGrade(String assignment) {
