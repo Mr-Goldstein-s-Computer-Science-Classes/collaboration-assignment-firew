c72c14cf363c449bd7324cef7c8309bb5ba5d338
126CC <113187762+126CC@users.noreply.github.com>
2024-02-22 13:50:26 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         Gradebook grades = new Gradebook();
+        grades.createStudent("Tyler");
+        grades.createStudent("James");
+        grades.createStudent("Ethan");
+        grades.createStudent("Scotty");
+        grades.createStudent("David");
+        grades.createStudent("Patrick");
+
+        grades.addAssignment("Stacks and Queues Station Activity");
+        grades.addAssignment("Stacks and queues programs");
+        grades.addAssignment("Stacks and queues quiz");
+        grades.addAssignment("Github Exploration");
+
         
     }
 }
